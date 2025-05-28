822d59b92a72e7394ff4e6c00d4ccea0b89f94cf
AF <gulabsinghrajput008@gmail.com>
2025-05-28 16:38:54 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -1,8 +1,7 @@
-import os
-from dotenv import load_dotenv
+# config.py
 
-load_dotenv()
+BOT_TOKEN = "8152839871:AAGHh16ficxHX49dA1vzRkfAdiGzKShvGXk"
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-MONGO_URL = os.getenv("MONGO_URL")
-ADMIN_ID = int(os.getenv("ADMIN_ID", 123456789))  # Default fallback
+MONGO_URL = "mongodb+srv://ravi:ravi12345@cluster0.hndinhj.mongodb.net/?retryWrites=true&w=majority"
+
+ADMIN_ID = 7170452349  # Your Telegram user ID
