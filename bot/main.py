dff719e5e49a383d0be9cb247f42781b7ccc2878
AF <gulabsinghrajput008@gmail.com>
2025-05-28 16:32:42 +0530

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -1,1 +1,14 @@
+from aiogram import Bot, Dispatcher, executor, types
+from bot.config import BOT_TOKEN
+from bot.handlers import start, get
+import logging
 
+bot = Bot(token=BOT_TOKEN)
+dp = Dispatcher(bot)
+
+start.register(dp)
+get.register(dp)
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    executor.start_polling(dp, skip_updates=True)
