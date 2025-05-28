d04cae47bb005f1d5b5d59877cdca26adf5426f4
AF <gulabsinghrajput008@gmail.com>
2025-05-28 16:33:22 +0530

    Update start.py

--- bot/handlers/start.py
+++ bot/handlers/start.py
@@ -1,1 +1,15 @@
-#
+from aiogram import types
+from aiogram.dispatcher import Dispatcher
+
+async def start_cmd(message: types.Message):
+    btn = types.InlineKeyboardMarkup().add(
+        types.InlineKeyboardButton("🔥 Join Channel", url="https://t.me/YourChannel")
+    )
+    await message.answer_photo(
+        photo="https://example.com/image.jpg",
+        caption="Welcome to the bot!",
+        reply_markup=btn
+    )
+
+def register(dp: Dispatcher):
+    dp.register_message_handler(start_cmd, commands=['start'])
